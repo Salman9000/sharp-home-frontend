11a72c959637fcc4e5644ca412285ac9e05d05ad
Muhammad Yahya <53046561+yahya801@users.noreply.github.com>
2021-05-06 05:27:34 +0500

    7days and 1Month chart made

--- component/Chartone.js
+++ component/Chartone.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {t} from 'react-native-tailwindcss';
 import {Searchbar, Title, Button} from 'react-native-paper';
 import Header from './Header';
@@ -9,8 +9,9 @@ import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
-
+import instance from '../helper';
 const ChartOne = props => {
+  const token = props.token;
   const [button1, setButton1] = useState(false);
   const [button2, setButton2] = useState(true);
   const [button3, setButton3] = useState(false);
@@ -38,73 +39,69 @@ const ChartOne = props => {
       },
     ],
   });
+  const getGraphs = async type => {
+    instance(token)
+      .get(`/v1/activity/` + type)
+      .then(value => {
+        console.log(value.data);
+        if (type === '1Month') {
+          setGraphData({
+            labels: value.data.result1Month.inputArray.labels,
+            datasets: [value.data.result1Month.inputArray.datasets],
+          });
+        } else if (type === '7Days') {
+          setGraphData({
+            labels: value.data.result7Days.inputArray.labels,
+            datasets: [value.data.result7Days.inputArray.datasets],
+          });
+        }
+        // console.log(value.data.result7days);
+      });
+  };
+  useEffect(() => {
+    getGraphs('7Days');
+  }, []);
 
   const pressButton1 = () => {
     setButton1(true);
     setButton2(false);
     setButton3(false);
-    setGraphData({
-      labels: ['12', '1', '2', '3', '4', '5'],
-      datasets: [
-        {
-          data: [
-            Math.random() * 100,
-            Math.random() * 100,
-            Math.random() * 100,
-            Math.random() * 100,
-            Math.random() * 100,
-            Math.random() * 100,
-          ],
-        },
-      ],
-    });
   };
   const pressButton2 = () => {
     setButton1(false);
     setButton2(true);
     setButton3(false);
-    setGraphData({
-      labels: [
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday',
-        'Sunday',
-      ],
-      datasets: [
-        {
-          data: [
-            Math.random() * 100,
-            Math.random() * 100,
-            Math.random() * 100,
-            Math.random() * 100,
-            Math.random() * 100,
-            Math.random() * 100,
-            Math.random() * 100,
-          ],
-        },
-      ],
-    });
+    getGraphs('7Days');
+    // setGraphData({
+    //   labels: [
+    //     'Monday',
+    //     'Tuesday',
+    //     'Wednesday',
+    //     'Thursday',
+    //     'Friday',
+    //     'Saturday',
+    //     'Sunday',
+    //   ],
+    //   datasets: [
+    //     {
+    //       data: [
+    //         Math.random() * 100,
+    //         Math.random() * 100,
+    //         Math.random() * 100,
+    //         Math.random() * 100,
+    //         Math.random() * 100,
+    //         Math.random() * 100,
+    //         Math.random() * 100,
+    //       ],
+    //     },
+    //   ],
+    // });
   };
   const pressButton3 = () => {
     setButton1(false);
     setButton2(false);
     setButton3(true);
-    setGraphData({
-      labels: ['Week1', 'Week2', 'Week3', 'Week4'],
-      datasets: [
-        {
-          data: [
-            Math.random() * 100,
-            Math.random() * 100,
-            Math.random() * 100,
-            Math.random() * 100,
-          ],
-        },
-      ],
-    });
+    getGraphs('1Month');
   };
   return (
     <View style={styles.container1}>
@@ -122,44 +119,46 @@ const ChartOne = props => {
             style={button2 ? styles.buttonOn : styles.buttonOff}
             mode={button2 ? 'contained' : 'text'}
             onPress={() => pressButton2()}>
-            7d
+            7 Days
           </Button>
           <Button
             style={button3 ? styles.buttonOn : styles.buttonOff}
             mode={button3 ? 'contained' : 'text'}
             onPress={() => pressButton3()}>
-            1month
+            1 month
           </Button>
         </View>
-        <LineChart
-          data={graphData}
-          width={Dimensions.get('window').width} // from react-native
-          height={220}
-          yAxisLabel="$"
-          yAxisSuffix="k"
-          yAxisInterval={1} // optional, defaults to 1
-          chartConfig={{
-            backgroundColor: '#e26a00',
-            backgroundGradientFrom: '#fb8c00',
-            backgroundGradientTo: '#ffa726',
-            decimalPlaces: 2, // optional, defaults to 2dp
-            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-            style: {
+        <ScrollView horizontal={true}>
+          <LineChart
+            data={graphData}
+            width={wp('150%')} // from react-native
+            height={hp('40%')}
+            yAxisLabel=""
+            yAxisSuffix="KW"
+            yAxisInterval={1} // optional, defaults to 1
+            chartConfig={{
+              backgroundColor: '#4050B5',
+              backgroundGradientFrom: '#4050B5',
+              backgroundGradientTo: '#4050C4',
+              decimalPlaces: 2, // optional, defaults to 2dp
+              color: (opacity = 0.5) => `rgba(255, 255, 255, ${opacity})`,
+              labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              style: {
+                borderRadius: 16,
+              },
+              propsForDots: {
+                r: '6',
+                strokeWidth: '2',
+                stroke: '#ffa726',
+              },
+            }}
+            bezier
+            style={{
+              marginVertical: 8,
               borderRadius: 16,
-            },
-            propsForDots: {
-              r: '6',
-              strokeWidth: '2',
-              stroke: '#ffa726',
-            },
-          }}
-          bezier
-          style={{
-            marginVertical: 8,
-            borderRadius: 16,
-          }}
-        />
+            }}
+          />
+        </ScrollView>
       </ScrollView>
       <Footer nav={props} />
     </View>
